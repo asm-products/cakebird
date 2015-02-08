174a2c2e557102e6af1d0d687aa27c7fbf1bacd5
Rhett Rogers <serenade.xs@gmail.com>
2015-02-08 16:59:13 -0700

    Fixed up multiple user creds in iOS

--- CakeBird/LoginLandingViewController.swift
+++ CakeBird/LoginLandingViewController.swift
@@ -11,63 +11,73 @@ import Accounts
 import SwifteriOS
 
 class LoginLandingViewController: SuperViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBAction func useWebCredentials(sender: AnyObject) {
-        loginToTwitterWeb()
-    }
-
-
-    @IBAction func useIOSCredentials(sender: AnyObject) {
-        loginToTwitterIOS()
-    }
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func useWebCredentials(sender: AnyObject) {
+    loginToTwitterWeb()
+  }
+  
+  
+  @IBAction func useIOSCredentials(sender: AnyObject) {
+    loginToTwitterIOS()
+  }
+  
+  
+  func loginToTwitterIOS() {
+    let user = UserManager.sharedInstance()
+    let actionSheet = UIAlertController(title: "Select an account...", message: nil, preferredStyle: .ActionSheet)
     
-    
-    func loginToTwitterIOS() {
-        let user = UserManager.sharedInstance()
-        if user == nil {
-            UserManager.requestAccounts { (error, accounts) -> Void in
-                if error != nil {
-                    println(error)
-                } else if let accountsUnwrap = accounts {
-                    println(accountsUnwrap)
-                    if accountsUnwrap.count < 1 {
-                        self.loginToTwitterWeb()
-                    }
-                    if accountsUnwrap.count > 1 {
-                        //Need to popover an account selector
-                    }
-                    if accountsUnwrap.count == 1 {
-                        UserManager.loginWithAccount(accountsUnwrap[0], complete: { (error) -> Void in
-                            self.pushStream()
-                        })
-                    }
-                } else {
-                    println("not granted")
-                }
+    if user == nil {
+      UserManager.requestAccounts { (error, accounts) -> Void in
+        if error != nil {
+          println(error)
+        } else if let accountsUnwrap = accounts {
+          if accountsUnwrap.count < 1 {
+            self.loginToTwitterWeb()
+          }
+          if accountsUnwrap.count > 1 {
+            //Need to popover an account selector
+            for account in accountsUnwrap {
+              actionSheet.addAction(UIAlertAction(title: account.accountDescription, style: .Default, handler: { (action) -> Void in
+                UserManager.loginWithAccount(account, complete: { (error) -> Void in
+                  self.pushStream()
+                })
+              }))
             }
+            actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+            self.presentViewController(actionSheet, animated: true, completion: nil)
+          }
+          if accountsUnwrap.count == 1 {
+            UserManager.loginWithAccount(accountsUnwrap[0], complete: { (error) -> Void in
+              self.pushStream()
+            })
+          }
         } else {
-            pushStream()
+          println("not granted")
         }
-        
+      }
+    } else {
+      pushStream()
     }
     
-    func loginToTwitterWeb() {
-        println("No twitter login.. Web login required")
-    }
-    
-    
-    func pushStream() {
-        performSegueWithIdentifier("landingToStream", sender: self)
-    }
+  }
+  
+  func loginToTwitterWeb() {
+    println("No twitter login.. Web login required")
+  }
+  
+  
+  func pushStream() {
+    performSegueWithIdentifier("landingToStream", sender: self)
+  }
 }
